ae073c7765513da1331098d052b27e85d185f10b
roman.novatorov <roman.novatorov@c-map.com>
2018-04-17 20:16:32 +0300

    - Update `entities.link`

--- src/vk_client/entities/link.py
+++ src/vk_client/entities/link.py
@@ -1,4 +1,5 @@
 import attr
+from vk_client.utils import offset_range
 from vk_client.entities._base import Entity, entity_manager
 
 
@@ -18,18 +19,19 @@ class Link(Entity):
 class LinkManager(entity_manager(Link)):
 
     def get(self, count=50):
-        response = self.vk.api.fave.getLinks(
-            count=count
-        )
-        return [
-            self(
-                id=item["id"],
-                url=item["url"],
-                title=item["title"],
-                description=item["description"],
-                photo_50=item["photo_50"],
-                photo_100=item["photo_100"],
-                photo_200=item["photo_200"]
+        for offset, count in offset_range(0, count, 50):
+            response = self.vk.api.fave.getLinks(
+                count=count
             )
-            for item in response["items"]
-        ]
+            for item in response["items"]:
+                yield self(
+                    id=item["id"],
+                    url=item["url"],
+                    title=item["title"],
+                    description=item["description"],
+                    photo_50=item["photo_50"],
+                    photo_100=item["photo_100"],
+                    photo_200=item["photo_200"]
+                )
+            if count > response["count"]:
+                raise StopIteration
