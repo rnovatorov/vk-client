35bbe5094755b24b7b2c07c531694e3fff1d44e2
Suenweek <par.adise@mail.ru>
2018-04-20 11:25:21 +0300

    - Formatting

--- access_token.py
+++ access_token.py
@@ -7,32 +7,25 @@ import requests
 import argparse
 
 
-@contextlib.contextmanager
-def serve_once(server_address, request_handler):
-    httpd = BaseHTTPServer.HTTPServer(server_address, request_handler)
-
-    thread = threading.Thread(target=httpd.handle_request)
-    thread.start()
-
-    yield httpd
-
-    thread.join()
-    httpd.server_close()
-
-
 def main(args):
-    request_args = {}
+    redirect_uri = "http://{args.host}:{args.port}".format(args=args)
 
     class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
         def do_GET(self):
-            request_args.update(self.get_request_args())
+            access_token = requests.get(make_get_token_url(
+                client_id=args.client_id,
+                client_secret=args.client_secret,
+                redirect_uri=redirect_uri,
+                code=self.get_request_args()["code"]
+            )).json()["access_token"]
+
+            with open("access_token.txt", "w") as f:
+                f.write(access_token)
 
         def get_request_args(self):
             query_params = urlparse.urlparse(self.path).query
             return dict(kv.split("=") for kv in query_params.split("&"))
-
-    redirect_uri = "http://{args.host}:{args.port}".format(args=args)
 
     with serve_once((args.host, args.port), RequestHandler):
         webbrowser.open(make_get_code_url(
@@ -41,15 +34,18 @@ def main(args):
             scope=args.scope
         ))
 
-    access_token = requests.get(make_get_token_url(
-        client_id=args.client_id,
-        client_secret=args.client_secret,
-        redirect_uri=redirect_uri,
-        code=request_args["code"]
-    )).json()["access_token"]
 
-    with open("access_token.txt", "w") as f:
-        f.write(access_token)
+@contextlib.contextmanager
+def serve_once(server_address, request_handler):
+    httpd = BaseHTTPServer.HTTPServer(server_address, request_handler)
+
+    thread = threading.Thread(target=httpd.handle_request)
+    thread.start()
+
+    yield
+
+    thread.join()
+    httpd.server_close()
 
 
 def make_get_code_url(client_id, redirect_uri, scope=""):
